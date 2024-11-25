d380189601a2f8111b95232e2ef9e8c643ec95af
JeongHyeon Jo <wjdgusdlekd3@yonsei.ac.kr>
2024-11-25 13:41:46 +0900

    refactor(test): refactor test

--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -4,50 +4,49 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
     @Test
     void 덧셈_테스트() {
-        // Arrange
+        // given
         Calculator calculator = Calculator.getInstance();
         calculator.reset();
 
-        // Act
+        // when
         calculator.addCalculatorEntry("1");
         calculator.addCalculatorEntry("2");
         calculator.addCalculatorEntry("3");
 
-        // Assert
+        // then
         assertEquals(BigInteger.valueOf(6), calculator.getResult());
     }
 
     @Test
     void 사용자_정의_구분자_테스트() {
-        // Arrange
+        // given
         Calculator calculator = Calculator.getInstance();
         calculator.reset();
-
-        // Act
         calculator.addCustomSet("#");
+
+        // when
         calculator.parsingCalculatorInput("4#5#6");
 
-        // Assert
+        // then
         assertEquals(BigInteger.valueOf(15), calculator.getResult());
     }
 
     @Test
     void 입력_없음_테스트() {
-        // Arrange
+        // given
         Calculator calculator = Calculator.getInstance();
         calculator.reset();
 
-        // Act
+        // when
         calculator.parsingCalculatorInput("");
 
-        // Assert
+        // then
         assertEquals(BigInteger.valueOf(0), calculator.getResult());
     }
-
 }
--- src/test/java/calculator/model/CustomTest.java
+++ src/test/java/calculator/model/CustomTest.java
@@ -5,48 +5,48 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomTest {
+
     @Test
     void 초기_구분자_테스트() {
-        // Arrange
+        // given
         Custom custom = new Custom();
 
-        // Act
+        // when
         Set<String> customSet = custom.getCustomSet();
 
-        // Assert
+        // then
         assertTrue(customSet.contains(DelimiterConstants.INITIAL_CUSTOM_COLON.getMessage()));
         assertTrue(customSet.contains(DelimiterConstants.INITIAL_CUSTOM_COMMA.getMessage()));
     }
 
     @Test
     void 사용자_정의_구분자_추가_테스트() {
-        // Arrange
+        // given
         Custom custom = new Custom();
 
-        // Act
+        // when
         custom.addCustomSet("#");
 
-        // Assert
+        // then
         assertTrue(custom.getCustomSet().contains("#"));
     }
 
     @Test
     void 리셋_후_초기화_테스트() {
-        // Arrange
+        // given
         Custom custom = new Custom();
-
-        // Act
         custom.addCustomSet("#");
+
+        // when
         custom.resetCustomSet();
         Set<String> customSet = custom.getCustomSet();
 
-        // Assert
+        // then
         assertTrue(customSet.contains(DelimiterConstants.INITIAL_CUSTOM_COLON.getMessage()));
         assertTrue(customSet.contains(DelimiterConstants.INITIAL_CUSTOM_COMMA.getMessage()));
         assertTrue(!customSet.contains("#"));
     }
-
 }
